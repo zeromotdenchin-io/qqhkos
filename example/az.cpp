6d5dfd84b9c571fa4bc24d7d306ea18e7b65b76f
sachtonyonline <87856018+sachtonyonline@users.noreply.github.com>
2021-08-30 13:30:24 +0700

    Create az.cpp

---
+++ example/az.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int az0 = 0, az1 = 1, az2 = 2, az3 = 3, az4 = 4, az5 = 5, az6 = 6, az7 = 7, az8 = 8, az9 = 9;  
+  cout << az0;
+  cout << az1;
+  cout << az2;
+  cout << az3;
+  cout << az4;
+  cout << az5;
+  cout << az6;
+  cout << az7;
+  cout << az8;
+  cout << az9;
+  return 0;
+}
