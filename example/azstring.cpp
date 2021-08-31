5423528858788a2e0bb128c1846fe48f5303b09d
sachtonyonline <87856018+sachtonyonline@users.noreply.github.com>
2021-08-31 22:54:49 +0700

    Create azstring.cpp

---
+++ example/azstring.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string AZ = "A, B, C, D, E, F, G, H, I ,J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z";
+  string az = "a, b, c, d, e, f, g, h ,i ,k , l, m, n, o, p, q, r, s, t, u, v, w, x, y, z";
+  cout << AZ;
+  cout << az;
+  return 0;
+}
