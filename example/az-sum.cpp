0d586b931e1ba78bc486573dba5b9b4053c97551
sachtonyonline <87856018+sachtonyonline@users.noreply.github.com>
2021-08-31 22:48:47 +0700

    Create az-sum.cpp

---
+++ example/az-sum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a, z;
+  int sum;
+  cout << "Type a number: ";
+  cin >> a;
+  cout << "Type another number: ";
+  cin >> z;
+  sum = a + z;
+  cout << "Sum is: " << sum;
+  return 0;
+}
